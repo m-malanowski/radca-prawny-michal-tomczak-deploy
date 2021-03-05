38810a9e3a6c714e6fb8bc401421c682b55c49fd
m-malanowski <info@mateusz-malanowski.pl>
2021-03-05 20:17:37 +0100

    fixed fixes fixinio fixazione per sempre 6

--- src/components/SEO.js
+++ src/components/SEO.js
@@ -4,6 +4,11 @@ import { Helmet } from "react-helmet"
 const SEO = ({title, description, schemaMarkup}) => {
   return <Helmet htmlAttributes={{ lang: "pl" }} title={title} >
     <meta name="description" content={description}/>
+    <meta property="og:url" content="radcaprawnytomczak.pl" />
+    <meta property="og:type" content="website"/>
+    <meta property="og:title" content={title} />
+    <meta property="og:description" content={description} />
+    <meta property="og:image" content="https://radcaprawnytomczak/static/facebook.jpg"/>
 
     {schemaMarkup &&
       <script type="application/ld+json">
